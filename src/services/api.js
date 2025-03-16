9e52e250ae6b62d1295d6715a2f818f1042adf97
Kartik0899 <jaybudhraja08@gmail.com>
2025-03-17 00:50:52 +0530

    cleanup commented code

--- src/services/api.js
+++ src/services/api.js
@@ -13,21 +13,10 @@ export const addTodo = async (todo) => {
   const response = await axios.post("https://dummyjson.com/todos/add", {
     todo: todo.todo,
     completed: todo.completed,
-    userId: 31, // 🔥 Adding a userId to fix the error
+    userId: 31,
   });
   return response.data;
 };
-// ✅ Add a new todo (Stored locally since API doesn't persist)
-// export const addTodo = async (todo) => {
-//   const newTodo = {
-//     id: Date.now(), // 🔥 Generate a unique ID locally
-//     todo: todo.todo,
-//     completed: todo.completed,
-//     userId: 1, // Required by DummyJSON
-//   };
-
-//   return newTodo; // Return new todo (don't use API since it's not persistent)
-// };
 
 // Update a todo
 export const updateTodo = async (id, updates) => {
@@ -42,5 +31,5 @@ export const deleteTodo = async (id) => {
 
 export const createTodo = async (todo) => {
   const response = await axios.post(API_URL + "/add", todo);
-  return response.data; // This is just a simulated response
+  return response.data;
 };
